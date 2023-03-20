6b75bd018f0887309c296bcd4a8e831bdd74944b
andreadldev <andrea.delisi2@gmail.com>
2023-03-20 16:45:06 +0100

    Aggiunta metodi

--- Product.cs
+++ Product.cs
@@ -1,31 +1,35 @@
-﻿public class Product
+﻿using System.Diagnostics;
+
+public class Product
 {
     public int id { get; } = new Random().Next(1000, 9999);
     public string name { get; set; }
     public string description { get; set; }
-    public string price { get; set; }
-    public int vat { get; set; }
+    public double price { get; set; }
+    public double vat { get; set; }
 
-    public Product(string name, string description, string price, int vat)
+    public Product(string name, string description, double price, double vat)
     {
         this.name = name;
         this.description = description;
         this.price = price;
         this.vat = vat;
     }
+
+    public string getPriceNoVAT()
+    {
+        var _price = price.ToString("0.00€");
+        return _price;
+    }
+    public string getPriceVAT()
+    {
+        var _priceWithVat = price + (price / 100) * vat + "€";
+        return _priceWithVat;
+    }
+
+    public string getExtendedName()
+    {
+        var extended = $"{id} - {name}";
+        return extended;
+    }
 };
-
-//1.
-//alla creazione di un nuovo prodotto il codice sia valorizzato con un numero random
-
-//2.
-//Il codice prodotto sia accessibile solo in lettura
-
-//3.
-//Gli altri attributi siano accessibili sia in lettura che in scrittura
-
-//4.
-//Il prodotto esponga sia un metodo per avere il prezzo base che uno per avere il prezzo comprensivo di iva
-
-//5.
-//Il prodotto esponga un metodo per avere il nome esteso, ottenuto concatenando codice + nome
--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
-﻿Product telefono = new Product("Samsung", "Telefono bello", "4,90", 5);
-Console.WriteLine(telefono.id);
-Console.WriteLine(telefono.name);
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Product phone = new Product("Samsung Galaxy S22", "Smartphone con sistema operativo Android", 438.00, 22);
+Console.WriteLine(phone.getExtendedName());
+Console.WriteLine($"\nCodice: {phone.id}");
+Console.WriteLine($"Nome prodotto: {phone.name}");
+Console.WriteLine($"Descrizione: {phone.description}");
+Console.WriteLine($"Prezzo: {phone.getPriceNoVAT()}");
+Console.WriteLine($"Prezzo + iva: {phone.getPriceVAT()}");
