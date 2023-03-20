b68c2de0ea7c676f073249e2964e385574fbe04b
andreadldev <andrea.delisi2@gmail.com>
2023-03-20 18:51:13 +0100

    Aggiunto array prodotti

--- Program.cs
+++ Program.cs
@@ -1,9 +1,20 @@
 ﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
 
-Product phone = new Product("Samsung Galaxy S22", "Smartphone con sistema operativo Android", 438.00, 22);
-Console.WriteLine(phone.getExtendedName());
-Console.WriteLine($"\nCodice: {phone.id}");
-Console.WriteLine($"Nome prodotto: {phone.name}");
-Console.WriteLine($"Descrizione: {phone.description}");
-Console.WriteLine($"Prezzo: {phone.getPriceNoVAT()}");
-Console.WriteLine($"Prezzo + iva: {phone.getPriceVAT()}");
+Product[] products = new Product[3];
+products[0] = new Product("Samsung Galaxy S22", "Smartphone Samsung con sistema operativo Android", 438.00, 22);
+products[1] = new Product("Asus Zenfone 9", "Smartphone Asus con sistema operativo Android", 492.00, 22);
+products[2] = new Product("Apple iPhone 14", "Smartphone Apple con sistema operativo iOS", 659.00, 22);
+
+for (int i = 0; i < products.Length; i++)
+{
+    if (i > 0)
+    {
+        Console.WriteLine();
+    }
+    Console.WriteLine(products[i].getExtendedName().ToUpper());
+    Console.WriteLine($"Codice: {products[i].id}");
+    Console.WriteLine($"Nome prodotto: {products[i].name}");
+    Console.WriteLine($"Descrizione: {products[i].description}");
+    Console.WriteLine($"Prezzo: {products[i].getPriceNoVAT()}");
+    Console.WriteLine($"Prezzo + iva: {products[i].getPriceVAT()}");
+} 
